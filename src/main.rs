c09c7d95f7b76c5a551c10b0268591022e005011
9gress0r <raevskijb@gmail.com>
2022-06-28 21:02:54 +0300

    Macros in the main file that will be deleted in the future, made them just not to write the full path all the time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 mod chess;
+use chess::*;
+
+// Macro to expand coord!(x, y) to Coordinate { row: x, col: y }
+macro_rules! coord {
+	($x:expr, $y:expr) => {
+		Coordinate { row: $x, col: $y }
+	};
+}
+
+// Macro to expand piece!(piece, color) to Piece { breed: Pieces::piece, color: Color::color }
+macro_rules! piece {
+	($piece:ident, $color:ident) => {
+		Piece { breed: Pieces::$piece, color: Color::$color }
+	};
+}
 
 fn main() {
 }
