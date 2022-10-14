bb747e3f0aa7166c9aa465f27a76856c86f4e15a
bgdnrvsky <raevskijb@gmail.com>
2022-10-14 23:26:40 +0200

    Some refactoring and logic improvements

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ use chess::*;
 
 // Macro to expand coord!(x, y) to Coordinate { row: x, col: y }
 macro_rules! coord {
-    ($x:expr, $y:expr) => {
-        Coordinate { row: $x, col: $y }
-    };
+	($x:expr, $y:expr) => {
+		Coordinate { row: $x, col: $y }
+	};
 }
 
 // Macro to expand piece!(piece, color) to Piece { breed: Pieces::piece, color: Color::color }
 macro_rules! piece {
-    ($piece:ident, $color:ident) => {
-        Piece {
-            breed: Pieces::$piece,
-            color: Color::$color,
-        }
-    };
+	($piece:ident, $color:ident) => {
+		Piece {
+			breed: Pieces::$piece,
+			color: Color::$color,
+		}
+	};
 }
 
-fn main() {}
+fn main() {
+	let board = Board::default();
+	board.draw();
+    println!("{}", board.get_fen());
+}
 
 #[cfg(test)]
 #[path = "./tests.rs"]
