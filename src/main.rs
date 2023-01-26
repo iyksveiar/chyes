6e95891b82af426b7d98d34f6ed45ecc48271088
bgdnvsky <raevskijb@gmail.com>
2023-01-26 15:58:54 +0100

    Simple example for future implementation

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,33 @@
+#[path ="./chess.rs"]
 mod chess;
-mod utils;
 
-/* TUI library */
-extern crate ncurses;
 use ncurses::*;
 
-// Macro to expand coord!(x, y) to Coordinate { row: x, col: y }
-#[allow(unused_macros)]
-macro_rules! coord {
-  ($x:expr, $y:expr) => {
-    Coordinate {
-      row: $x, col: $y
-    }
-  };
-}
-
-// Macro to expand piece!(piece, color) to Piece { breed: Pieces::piece, color: Color::color }
-#[allow(unused_macros)]
-macro_rules! piece {
-  ($piece:ident, $color:ident) => {
-    Piece {
-      breed: Pieces::$piece,
-      color: Color::$color
-    }
-  };
-}
-
-fn ncurses_draw_board(board: &chess::Board, row: i32, col: i32) {
-    let mut current_row = row;
-    let repr: [String; 8] = utils::transform_chess_board_to_strings(board);
-
-    while current_row != row + 8 {
-        mv(current_row, col);
-
-        /* Printing row */
-        addstr(repr[(current_row - row) as usize].as_str());
-
-        current_row += 1;
-    }
-}
-
 fn main() {
-  let board = Board::default();
-  board.draw();
-  println!("{}", board.get_fen());
+    // Initialize ncurses
+    initscr();
+    noecho();
+
+    // Get the size of the terminal window
+    let mut rows = 0;
+    let mut cols = 0;
+    getmaxyx(stdscr(), &mut rows, &mut cols);
+
+    // Print "Hello World!" in the center of the screen
+    mvprintw(rows/2, (cols-11)/2, "Hello World!");
+
+    // Refresh the screen to make the changes visible
+    refresh();
+
+    // Wait for the user to press a key before exiting
+    loop {
+        let ch = getch();
+        if ch == 'q' as i32 {
+            break;
+        }
+    }
+
+    // Cleanup ncurses
+    endwin();
 }
 
-#[cfg(test)]
-#[path = "./tests.rs"]
-mod tests;
