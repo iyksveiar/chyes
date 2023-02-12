e6514a0e1e172a29abf0c07037b34ac625ea170d
bgdnvsky <raevskijb@gmail.com>
2023-02-12 13:59:57 +0100

    Main file update due to the lib change

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,64 @@
-use ncurses::*;
+use std::{io, thread, time::Duration};
 
-fn main() {
-  // Initialize ncurses
-  initscr();
-  noecho();
+use chyes::Board;
+use crossterm::{
+  event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+  execute,
+  terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
+};
+use tui::{
+  backend::CrosstermBackend,
+  layout::{Constraint, Direction, Layout, Margin, Rect},
+  widgets::{Block, Borders, Widget},
+  Terminal
+};
 
-  // Get the size of the terminal window
-  let mut rows = 0;
-  let mut cols = 0;
-  getmaxyx(stdscr(), &mut rows, &mut cols);
+fn main() -> Result<(), io::Error> {
+  // setup terminal
+  enable_raw_mode()?;
+  let mut stdout = io::stdout();
+  execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+  let backend = CrosstermBackend::new(stdout);
+  let mut terminal = Terminal::new(backend)?;
 
-  // Print "Hello World!" in the center of the screen
-  mvprintw(rows / 2, (cols - 11) / 2, "Hello World!");
+  let board = Board::default();
 
-  // Refresh the screen to make the changes visible
-  refresh();
+  terminal.draw(|f| {
+    let size = f.size();
+    let block = Block::default().title("Chyes").borders(Borders::ALL);
+    f.render_widget(block, size);
+    f.render_widget(
+      board,
+      size.inner(&Margin {
+        horizontal: 10,
+        vertical:   10
+      })
+    );
+  })?;
 
-  // Wait for the user to press a key before exiting
   loop {
-    let ch = getch();
-    if ch == 'q' as i32 {
-      break
+    match event::read()? {
+      Event::Key(event) => match event.code {
+        KeyCode::Char('q') => break,
+        _ => {}
+      },
+      Event::Mouse(_) => (),
+      Event::Resize(width, height) => {
+        {
+          terminal.resize(Rect::new(0, 0, width, height))?;
+        };
+      }
     }
   }
 
-  // Cleanup ncurses
-  endwin();
+  // restore terminal
+  disable_raw_mode()?;
+  execute!(
+    terminal.backend_mut(),
+    LeaveAlternateScreen,
+    DisableMouseCapture
+  )?;
+  terminal.show_cursor()?;
+
+  Ok(())
 }
